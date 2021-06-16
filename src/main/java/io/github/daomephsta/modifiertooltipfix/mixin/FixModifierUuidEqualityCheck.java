184a70868c93697dd0a3e5b8540c79a6cd532835
Daomephsta <dtnilfisk@gmail.com>
2021-06-17 00:54:17 +1200

    Fixed checking attribute modifier name instead of attribute name

--- src/main/java/io/github/daomephsta/modifiertooltipfix/mixin/FixModifierUuidEqualityCheck.java
+++ src/main/java/io/github/daomephsta/modifiertooltipfix/mixin/FixModifierUuidEqualityCheck.java
@@ -5,44 +5,45 @@ import java.util.UUID;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-@Mixin(ItemStack.class)
+@Mixin(EntityAttributeModifier.class)
 public class FixModifierUuidEqualityCheck
 {	
     /** 
      * For UUIDs that are equals()-equal to either modifier UUID & modify the corresponding attribute, 
      * returns the modifier UUID instance instead. This effectively replaces the == check with an equals() check.
      */
-	@Redirect(method = "getTooltip", 
-	    at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttributeModifier;getId()Ljava/util/UUID;"))
-	UUID internAttackModifierUUIDs(EntityAttributeModifier attributeModifier)
+	@ModifyVariable(method = "fromTag", at = @At(value = "INVOKE_ASSIGN", 
+	    target = "net/minecraft/nbt/CompoundTag.getUuid(Ljava/lang/String;)Ljava/util/UUID;"))
+	private static UUID internAttackModifierUUIDs(UUID uuid, CompoundTag tag)
 	{
-	    if (ItemAccessors.getAttackDamageModifierUuid().equals(attributeModifier.getId()) && 
-	        modifiesAttribute(attributeModifier, EntityAttributes.GENERIC_ATTACK_DAMAGE))
+	    if (ItemAccessors.getAttackDamageModifierUuid().equals(uuid) && 
+	        modifiesAttribute(tag, EntityAttributes.GENERIC_ATTACK_DAMAGE))
 	    {
 	        return ItemAccessors.getAttackDamageModifierUuid();
 	    }
-	    if (ItemAccessors.getAttackSpeedModifierUuid().equals(attributeModifier.getId()) &&
-	        modifiesAttribute(attributeModifier, EntityAttributes.GENERIC_ATTACK_SPEED))
+	    if (ItemAccessors.getAttackSpeedModifierUuid().equals(uuid) &&
+	        modifiesAttribute(tag, EntityAttributes.GENERIC_ATTACK_SPEED))
         {
             return ItemAccessors.getAttackSpeedModifierUuid();
         }
-	    return attributeModifier.getId();
+	    return uuid;
 	}
 
 	@Unique
-    private boolean modifiesAttribute(EntityAttributeModifier attributeModifier, EntityAttribute attribute)
+    private static boolean modifiesAttribute(CompoundTag tag, EntityAttribute attribute)
     {
-        Identifier id = Registry.ATTRIBUTE.getId(attribute);
-        return id.toString().equals(attributeModifier.getName()) || 
-            (id.getNamespace().equals("minecraft") && id.getPath().equals(attributeModifier.getName()));
+        Identifier test = Registry.ATTRIBUTE.getId(attribute);
+        String modifies = tag.getString("AttributeName");
+        return test.toString().equals(modifies) || 
+            (test.getNamespace().equals("minecraft") && test.getPath().equals(modifies));
     }
 }
